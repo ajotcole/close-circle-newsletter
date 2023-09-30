01890e434b4f13cda381fa89f91c0cde80f8acfd
Alexander James Cole <aj@co-le.com>
2023-09-30 22:11:39 +0200

    feat: rebuild base project as a spring project

---
+++ closecirclenewsletter/src/main/java/com/ajotcole/closecirclenewsletter/ClosecirclenewsletterApplication.java
@@ -0,0 +1,13 @@
+package com.ajotcole.closecirclenewsletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosecirclenewsletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosecirclenewsletterApplication.class, args);
+	}
+
+}
---
+++ closecirclenewsletter/src/test/java/com/ajotcole/closecirclenewsletter/ClosecirclenewsletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajotcole.closecirclenewsletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosecirclenewsletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
