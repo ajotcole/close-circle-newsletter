608fc438437084e4c9e4baef26db530d8b38e707
Alexander James Cole <alexander.cole@taxdoo.com>
2023-11-10 15:19:31 +0100

    feat: added first working integration test

---
+++ src/test/java/com/ajotcole/closecirclenewsletter/SendMailMutationIT.java
@@ -0,0 +1,63 @@
+package com.ajotcole.closecirclenewsletter;
+
+import net.minidev.json.JSONObject;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SendMailMutationIT {
+
+    private final MockMvc mockMvc;
+
+    @Autowired
+    public SendMailMutationIT(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    public void firstSampleTest() throws Exception {
+        String query = "query allBooks() {" +
+                " allBooks {" +
+                "  id" +
+                "  title" +
+                " }" +
+                "}";
+        JSONObject variables = new JSONObject();
+
+        MvcResult mvcResult = mockMvc.perform(post("/graphql")
+                        .content(generateRequest(query, variables))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(request().asyncStarted())
+                .andExpect(request().asyncResult(notNullValue()))
+                .andReturn();
+
+        mockMvc.perform(asyncDispatch(mvcResult))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    private String generateRequest(String query, JSONObject variables) throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("query", query);
+        if (variables != null) {
+            jsonObject.put("variables", variables);
+        }
+        return jsonObject.toString();
+    }
+}
